e3ce7d49ed5c0b860e4da4d6bf54aa1f8b38a751
Daniel Joseph Antrim <daniel.joseph.antrim@cern.ch>
2017-11-02 10:49:39 +0100

    add bash handling of new functions

--- _notes.sh
+++ _notes.sh
@@ -20,6 +20,26 @@ function nl {
 
 }
 
+function nld {
+
+    if [[ ! ${1} ]]; then
+        notes --list-by-date
+    else
+        notes --list-by-date |tail -${1}
+    fi
+
+}
+
+function nos {
+
+    if [[ ! ${1} ]]; then
+        echo "provided empty string"
+    else
+        notes --open ${1}
+    fi
+
+}
+
 function nlo {
 
     notes --open-list
